ee2102740f76413cd2f083de51df2a26b4c5646f
少年锦时 <283448189@qq.com>
2019-09-04 16:44:41 +0800

    change

--- src/renderer/views/flowgl.vue
+++ src/renderer/views/flowgl.vue
@@ -42,6 +42,7 @@ export default {
   methods: {
     setOptions(data) {
       this.chart.setOption({
+        backgroundColor: "#000",
         visualMap: {
           show: false,
           min: valMin,
